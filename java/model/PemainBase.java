d3ec1b31fa0565ee5c8396962cc077a8b7453d63
ghanimp <ghanimandala07@gmail.com>
2025-09-29 20:01:04 +0800

    Add files via upload

---
+++ java/model/PemainBase.java
@@ -0,0 +1,30 @@
+package model;
+
+public abstract class PemainBase {
+    private String nama;
+    private String klub;
+
+    public PemainBase(String nama, String klub) {
+        this.nama = nama;
+        this.klub = klub;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKlub() {
+        return klub;
+    }
+
+    public void setKlub(String klub) {
+        this.klub = klub;
+    }
+
+    // abstraksi
+    public abstract void infoPosisi();
+}
